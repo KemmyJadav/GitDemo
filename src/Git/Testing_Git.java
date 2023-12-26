4c634a9599e65ee838bc4cb9b05995f0b0c2666b
Kemmy <kemmy.j@techovarya.com>
2023-12-26 12:24:12 +0530

    first commit

---
+++ src/Git/Testing_Git.java
@@ -0,0 +1,12 @@
+package Git;
+
+public class Testing_Git {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Only for testing");
+		System.out.println("Project uploaded from Git suceessfully");
+	}
+
+}
